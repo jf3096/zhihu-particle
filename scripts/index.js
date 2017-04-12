5c0e8d482d6ff6d92bcb254fb3251f22211829c9
°¬Â× <529360897@qq.com>
2017-04-12 21:09:54 +0800

    since gh-page handled exception, change it to console.error

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,6 @@ const path = require('path');
 const targetDirectory = path.join(__dirname, '../demo/zhihu-inferno/www');
 ghPages.publish(targetDirectory, (err) => {
     if (err) {
-        throw err;
+        console.log(err);
     }
 });
